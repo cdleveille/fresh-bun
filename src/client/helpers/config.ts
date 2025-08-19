708e91a7d56e5f30f436f12fd2446203d5f55fda
Chris Leveille <cdleveille@gmail.com>
2025-08-18 22:51:39 -0400

    updates

--- src/client/helpers/config.ts
+++ src/client/helpers/config.ts
@@ -2,5 +2,5 @@ import { Env } from "@/shared/constants";
 
 export const Config = {
   IS_PROD: import.meta.env.MODE === Env.Production,
-  PORT: Number.parseInt(import.meta.env.PORT ?? window.location.port),
+  PORT: Number(import.meta.env.PORT ?? window.location.port),
 };
--- src/server/config.ts
+++ src/server/config.ts
@@ -1,3 +1,3 @@
-const PORT = process.env.PORT ? Number.parseInt(process.env.PORT) : 3000;
+const PORT = Number(process.env.PORT) || 3000;
 
 export const Config = { PORT };
