526bde7d05b176cf9d0ec8baed319a170c5e2d4c
Chris Leveille <cdleveille@gmail.com>
2025-09-27 23:49:54 -0400

    ws api fixes

--- src/client/hooks/useApi.ts
+++ src/client/hooks/useApi.ts
@@ -37,11 +37,11 @@ export const useWsHello = ({
   onSuccess,
 }: {
   body: Static<typeof schema.api.hello.ws.body>;
-  onSuccess: TOnSuccess<typeof schema.api.hello.ws.response>;
+  onSuccess: TOnSuccess<(typeof schema.api.hello.ws.response)[200]>;
 }) => {
   return useWs({
     handler: apiClient.ws.hello,
-    onSuccess: ({ data }) => data && onSuccess(data[200]),
+    onSuccess: ({ data }) => data && onSuccess(data),
     body,
   });
 };
--- src/server/api.ts
+++ src/server/api.ts
@@ -8,9 +8,8 @@ export const api = new Elysia({ prefix: "/api" })
     "/hello",
     c => {
       const { message } = c.query;
-      const { method } = c.request;
-      console.log(`${method} ${c.route}${message ? ` "${message}"` : ""}`);
-      return { message: `${method}: hello from bun!` };
+      console.log(`GET /api/hello${message ? ` "${message}"` : ""}`);
+      return { message: "GET: hello from bun!" };
     },
     schema.api.hello.get,
   )
@@ -18,17 +17,15 @@ export const api = new Elysia({ prefix: "/api" })
     "/hello",
     c => {
       const { message } = c.body;
-      const { method } = c.request;
-      console.log(`${c.request.method} ${c.route} "${message}"`);
-      return { message: `${method}: hello from bun!` };
+      console.log(`POST /api/hello "${message}"`);
+      return { message: "POST: hello from bun!" };
     },
     schema.api.hello.post,
   )
   .ws("/hello", {
     message(ws, { message }) {
-      const method = "WS";
-      console.log(`${method} ${ws.data.path} "${message}"`);
-      ws.send({ message: `${method}: hello from bun!` });
+      console.log(`WS /api/hello "${message}"`);
+      ws.send({ message: "WS: hello from bun!" });
     },
     idleTimeout: WS_TIMEOUT,
     ...schema.api.hello.ws,
