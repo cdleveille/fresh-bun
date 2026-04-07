24a5a075513fcabe730f6e31ced40a891abc320c
Chris Leveille <cdleveille@gmail.com>
2026-04-07 00:24:09 -0400

    scalar mobile display fix

--- src/client/routes/api.tsx
+++ src/client/routes/api.tsx
@@ -2,15 +2,12 @@ import { createFileRoute } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/api")({
   component: () => (
-    <iframe
-      src="/api/docs"
-      title="Swagger UI"
-      style={{
-        width: "100%",
-        height: "calc(100vh - 3.25rem)",
-        border: "none",
-        display: "block",
-      }}
-    />
+    <div className="scalar-wrapper">
+      <iframe
+        src="/api/docs"
+        title="Swagger UI"
+        style={{ width: "100%", height: "100%", border: "none", display: "block" }}
+      />
+    </div>
   ),
 });
--- src/shared/declarations.d.ts
+++ src/shared/declarations.d.ts
@@ -1,3 +1,5 @@
+declare module "*.css" {}
+
 declare module "*.svg" {
   const component: React.FC<React.SVGProps<SVGSVGElement>>;
   export default component;
