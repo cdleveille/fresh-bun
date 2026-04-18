66c7dd6214e1427a479f1f45f1c20b6bf2531d3a
Chris Leveille <cdleveille@gmail.com>
2026-04-17 20:42:00 -0400

    updated deps, refactoring

---
+++ src/client/components/Api.tsx
@@ -0,0 +1,14 @@
+export const Api = () => {
+  return (
+    <iframe
+      src="/api/docs"
+      title="Swagger UI"
+      style={{
+        width: "100%",
+        height: "calc(100vh - 3.25rem)",
+        border: "none",
+        display: "block",
+      }}
+    />
+  );
+};
--- src/client/routes/api.tsx
+++ src/client/routes/api.tsx
@@ -1,16 +1,7 @@
 import { createFileRoute } from "@tanstack/react-router";
 
+import { Api } from "@/client/components/Api";
+
 export const Route = createFileRoute("/api")({
-  component: () => (
-    <iframe
-      src="/api/docs"
-      title="Swagger UI"
-      style={{
-        width: "100%",
-        height: "calc(100vh - 3.25rem)",
-        border: "none",
-        display: "block",
-      }}
-    />
-  ),
+  component: Api,
 });
