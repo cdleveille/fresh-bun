4859ef61de6557e0fd1a3bdec1f60a714df1a8d6
Chris Leveille <cdleveille@gmail.com>
2026-05-06 02:49:07 -0400

    better error message extraction

--- src/client/components/Error.tsx
+++ src/client/components/Error.tsx
@@ -1,4 +1,6 @@
-export const ErrorBoundary = ({ error }: { error: Error }) => {
+export const ErrorBoundary = ({ error }: { error: unknown }) => {
+  const message = getErrorMessage(error);
+
   return (
     <div
       role="alert"
@@ -12,10 +14,21 @@ export const ErrorBoundary = ({ error }: { error: Error }) => {
       }}
     >
       <h1>Error!</h1>
-      <div style={{ color: "red", fontFamily: "monospace", fontSize: "1rem" }}>{error.message}</div>
+      <div style={{ color: "red", fontFamily: "monospace", fontSize: "1rem" }}>{message}</div>
       <a href="/">Home</a>
     </div>
   );
 };
 
 export const NotFound = () => <ErrorBoundary error={new Error("Not Found")} />;
+
+const getErrorMessage = (error: unknown) => {
+  if (error instanceof Error) return error.message;
+  if (typeof error === "string") return error;
+  if (error && typeof error === "object") {
+    if ("message" in error) return String((error as { message: unknown }).message);
+    if ("error" in error && typeof (error as { error: unknown }).error === "string")
+      return (error as { error: string }).error;
+  }
+  return "An unknown error occurred.";
+};
